1d9daa2464999ef0b9f39d4ba228f3a5d0cd05c4
Ernesto-07 <a01710199@tec.mx>
2025-08-29 23:46:58 -0600

    Add files via upload

---
+++ avance2.py
@@ -0,0 +1,8 @@
+# ejemplo básico avance 2
+subir = 66.47 + (13.75 * 85) + (5 * 178) - (6.74 * 18) + 500
+bajar = 66.47 + (13.75 * 85) + (5 * 178) - (6.74 * 18) - 500
+mantener = 66.47 + (13.75 * 85) + (5 * 178) - (6.74 * 18)
+
+print("Calorías para subir de peso: %.3f" % subir)
+print("Calorías para bajar de peso: %.3f" % bajar)
+print("Calorías para mantener peso: %.3f" % mantener)
