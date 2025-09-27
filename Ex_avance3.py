610cb2c06cfab3751941048df868722d8bad531e
Ernesto-07 <a01710199@tec.mx>
2025-09-26 22:12:20 -0600

    Update Ex_avance5.py

--- Ex_avance3.py
+++ Ex_avance3.py
@@ -1,3 +1,10 @@
+opcion = input("Elige tu opcion:")
+uno = "Pedir datos del usuario"
+dos = "Pedir al usuario el objetivo"
+tres = "Establecer cantidad ideal de calorias"
+cuatro = "Registrar datos de sus comidas"
+cinco = "Total calorias "
+
 
 def calorias_necesarias (objetivo,P,A,E):
 	if objetivo == "subir":
@@ -6,8 +13,21 @@ def calorias_necesarias (objetivo,P,A,E):
 		return 66.47 + (13.75 * P) + (5 * A) - (6.74 * E) - 500
 	elif objetivo == "mantener":
 		return 66.47 + (13.75 * P) + (5 * A) - (6.74 * E)
-        
+	
+total_calorias = 0
+	
+if opcion == "cuatro":
+    continuar = "si"
+    while continuar == "si":
+        calorias = float(input("Ingresa las calorías de la comida:"))
+        total_calorias = total_calorias + calorias
+        print ("Tus calorias hasta ahora son:",total_calorias)
+        continuar = input ("quieres seguir registrando comidas (si/no):")
+      
+           
 print (calorias_necesarias("subir",85,178,18))
 print (calorias_necesarias("bajar",85,178,18))
 print (calorias_necesarias("mantener",85,178,18))
+print ("calorias del dia:", total_calorias)
+
 	
